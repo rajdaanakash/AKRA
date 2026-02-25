f5eb9c29c458eef3c1f9ed6cdd054db7e3a071a9
rajdaanakash <rajdaanakash@gmail.com>
2026-02-25 14:55:30 +0000

    EVA Cloud Sync: 2026-02-25 14:55:30

---
+++ history/ai_responses/marvel/togglejs_145530.js
@@ -0,0 +1,10 @@
+// JavaScript Code
+// Toggle Sidebar Functionality
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const container = document.querySelector('.container');
+
+menuIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('small-sidebar');
+  container.classList.toggle('large-container');
+});
